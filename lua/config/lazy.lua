a907facfab329a43ff5eb13f11f5631a3f18959c
Dtzibell <zaleniakas.tauras@gmail.com>
2025-05-28 21:01:10 +0200

    add telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,3 +52,10 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+-- telescope keybinds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
