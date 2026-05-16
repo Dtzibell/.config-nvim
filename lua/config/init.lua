b7d01aaae56436e1e46ed469e666f9a54787bb89
Dtzibell <zaleniakas.tauras@gmail.com>
2026-05-16 21:49:52 +0200

    a

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -7,5 +7,5 @@ require('vim._core.ui2').enable({})
 gh = function(x) return "https://www.github.com/" .. x end
 -- require("plugins")
 
-vim.cmd("colorscheme synthweave")
+-- vim.cmd("colorscheme synthweave")
 
